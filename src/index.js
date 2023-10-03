198d33b7bf67346af9822dc0851beb1141ea0b77
Stanley SkimZ Kimambo <Stanleykimambo@gmail.com>
2023-10-03 20:18:37 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('create-task-form');
+
+  form.addEventListener('submit', (e) => e.preventDefault());
+
+  let txtBox = document.querySelector('input[type = "text"]');
+  let btnSub = document.querySelector('input[type = "submit"]');
+  let list = document.getElementById('tasks');
+
+  function listAppending() {
+    let li = document.createElement('li');
+    li.textContent = txtBox.value;
+    list.appendChild(li);
+  };
+
+  btnSub.addEventListener('click', listAppending);
 });
